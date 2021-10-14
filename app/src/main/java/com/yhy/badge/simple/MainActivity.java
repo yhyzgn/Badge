861a4ca23564b0cd0ca294731e7c2592207a1a6b
颜洪毅 <yhyzgn@gmail.com>
2021-10-14 15:06:20 +0800

    jitpack

--- app/src/main/java/com/yhy/badge/simple/MainActivity.java
+++ app/src/main/java/com/yhy/badge/simple/MainActivity.java
@@ -1,11 +1,12 @@
 package com.yhy.badge.simple;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.yhy.badge.Badge;
 import com.yhy.badge.BadgeRadioButton;
