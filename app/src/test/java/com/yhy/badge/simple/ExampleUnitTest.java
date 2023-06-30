84baeeb206b62e1aaba138dbf234fdc5f96e09f0
颜洪毅 <yhyzgn@gmail.com>
2023-06-30 11:02:26 +0800

    fix:Encoding Error

--- app/src/test/java/com/yhy/badge/simple/ExampleUnitTest.java
+++ app/src/test/java/com/yhy/badge/simple/ExampleUnitTest.java
@@ -1,6 +1,6 @@
 package com.yhy.badge.simple;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 
 import static org.junit.Assert.*;
 
---
+++ badge-annotation/src/main/java/android/view/View.java
@@ -0,0 +1,13 @@
+package android.view;
+
+/**
+ * View Class Holder Of Annotatioin
+ * <p>
+ * Created on 2023-06-30 10:56
+ *
+ * @author 颜洪毅
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class View {
+}
